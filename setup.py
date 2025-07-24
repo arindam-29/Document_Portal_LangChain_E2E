e8177406f4ad05100827a472ab98dcebd3dc8ead
Arindam Choudhury <arindam.choudhury.email@gmail.com>
2025-07-23 21:27:24 -0400

    Added Project Setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import  setup, find_packages
+
+setup(
+    name="Agentic AI Document Portal Using LangGraph",
+    author="Arindam Choudhury",
+    version="1.0",
+    packages=find_packages(),
+)
+
