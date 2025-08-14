2a1f7a750e724c7a12b698036bddaaab41aa898f
Arindam Choudhury <arindam.choudhury.email@gmail.com>
2025-08-13 23:42:06 -0400

    data ingestion and retriever code updated for multi doc chat

---
+++ src/multi_document_chat/retrieval.py
@@ -0,0 +1,22 @@
+class ConversationalRAG:
+    def __init__(self, vectorstore, llm, conversation_memory):
+        pass
+
+    def load_retriever_from_faiss(self):
+        # Logic to load and return the retriever
+        pass
+
+    def invoke(self):
+        pass
+
+    def _load_llm(self):
+        # Logic to load and return the LLM
+        pass
+
+    @staticmethod
+    def _format_docs(docs):
+        # Logic to format the documents
+        pass
+
+    def _build_lcel_chain(self):
+        pass
